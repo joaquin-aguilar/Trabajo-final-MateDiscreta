5aedb0ec5403bc4d1f0da4c48deb6730522ec68f
Joaquin Aguilar <aguilarjoaquinandre@gmail.com>
2025-04-11 19:23:28 -0500

    Añadido: Creacion, impresion y liberacion de matrices booleanas

--- MatrizBooleana.h
+++ MatrizBooleana.h
@@ -1,8 +1,41 @@
 #ifndef MATRIZ_BOOLEANA_H
 #define  MATRIZ_BOOLEANA_H
 
-bool** crearMatrizBooleana();
+#include <stdlib.h>
+#include <stdbool.h>
 
+bool** crearMatrizBooleana(short, short);
+void imprimirMatrizBooleana(bool**, short, short);
 
+bool** crearMatrizBooleana(short filas, short columnas)
+{
+    bool** matriz = (bool**) malloc(sizeof(void*) * filas);
+    for(short i = 0; i < filas; i++)
+        matriz[i] = (bool*) malloc(sizeof(void*) * columnas);
+    
+    for(short i = 0; i < filas; i++)
+        for(short j = 0; j < filas; j++)
+            matriz[i][j] = 0;
+    return matriz;
+}
 
-#endif MATRIZ_BOOLEANA_H
+void liberarMatriz(bool** matriz, short filas)
+{
+    for (int i = 0; i < filas; i++) 
+        free(matriz[i]); 
+
+    free(matriz); 
+}
+
+void imprimirMatrizBooleana(bool** matriz, short filas, short columnas)
+{
+    for(short i = 0; i < filas; i++)
+    {
+        printf("|");
+        for(short j = 0; j < filas; j++)
+            printf("%d ", matriz[i][j]);   
+        printf("|\n");
+    }
+}
+
+#endif // !MATRIZ_BOOLEANA_H
