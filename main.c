1d57db233b3063c9e62bad0a99829a838de4dd8c
Joaquin Aguilar <aguilarjoaquinandre@gmail.com>
2025-04-17 16:32:45 -0500

    reubicado: matriz_booleana.h

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <time.h>
 
 #include "MatrizBooleana.h"
 
 int main(int argc, char** argv)
 {
+    srand(time(NULL));
     if (argc != 3)
     {
         printf("No se obtuvieron los parametros esperados para la matriz, programa finalizado!\n");
@@ -16,9 +18,15 @@ int main(int argc, char** argv)
     int filas = atoi(argv[1]);
     int columnas = atoi(argv[2]);
     bool** matriz = crearMatrizBooleana(filas, columnas);
+    autoAsignarMatrizBooleana(matriz, filas, columnas);    
+    bool** matrizTraspuesta = obtenerTraspuesta(matriz, filas, columnas);
+
 
     imprimirMatrizBooleana(matriz, filas, columnas);
+    printf("\ntraspuesta:\n");
+    imprimirMatrizBooleana(matrizTraspuesta, columnas, filas);
+
     liberarMatriz(matriz, filas);
-    
+    liberarMatriz(matrizTraspuesta, filas);
     return 0; 
 }
